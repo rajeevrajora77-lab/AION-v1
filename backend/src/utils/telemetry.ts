638c958f878b13fd68c59b12410112cf7a219548
rajeevrajora77-lab <rajeev.rajora.77@gmail.com>
2026-04-18 15:24:27 +0530

    fix: simplify telemetry.ts - remove broken console SpanExporter
    Add a newline at the end of telemetry.ts

--- backend/src/utils/telemetry.ts
+++ backend/src/utils/telemetry.ts
@@ -6,3 +6,4 @@ export const captureTraceId = (req: any, res: any, next: any) => {
   const traceId = req.headers['x-request-id'] || `req_${Date.now()}`;
   traceStorage.run(traceId as string, () => next());
 };
+ 
